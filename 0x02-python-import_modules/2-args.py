58ac77e21ab0f3a30b8de61403a94795355ad78a
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-04-28 20:39:50 +0000

    puntos 2 y 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+  from sys import argv
+  count = len(argv)
+
+  if count == 1:
+    print("{}".format(count - 1), "arguments.")
+
+  elif count == 2:
+    print("{}".format(count - 1), "argument:")
+    print("{}: {}".format(count - 1, argv[1]))
+  
+  else:
+    for i in range(count):
+      if i == 0:
+        print("{} {}:".format(count - 1, "arguments:"))
+      else:
+        print("{}: {}".format(i, argv[i]))
