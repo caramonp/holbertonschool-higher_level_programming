dfa785ea69bb4e0f87b4d36733d804aeb8432b2e
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-06 23:58:53 +0000

    len and firts character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!usr/bin/python3
-
 def multiple_returns(sentence):
-    sentence_len = len(sentence)
-    if (sentence_len == 0):
-        return (0, None)
-    return (sentence_len, sentence[0])
+    if not sentence:
+        sentence = None
+    if sentence:
+        sentence_len = len(sentence)
+    else:
+        sentence_len = 0
+    return (sentence_len, sentence if not sentence else sentence[:1])
