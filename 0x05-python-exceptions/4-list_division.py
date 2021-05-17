a412ac017457f65abe59daf14bc432abcbb8ddab
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-17 20:14:04 +0000

    number= 0 in the exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     number = 0
     result = []
-    for number in range(list_length):
+    while number < list_length:
         try:
             res = (my_list_1[number]/my_list_2[number])
         except TypeError:
