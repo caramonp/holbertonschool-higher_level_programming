396e74abaf7ea286648ab24256c148c79fc7b433
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-17 20:12:24 +0000

    number= 0 in the exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,10 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             res = (my_list_1[number]/my_list_2[number])
         except TypeError:
+            number = 0
             print("wrong type")
         except ZeroDivisionError:
+            number = 0
             print("division by 0")
         except IndexError:
+            number = 0
             print("out of range")
         finally:
             number += 1
