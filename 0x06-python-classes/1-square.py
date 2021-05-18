d061aec4a0e9654e60e8347d75bb468046d12a16
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-18 21:51:10 +0000

    commits

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-class my_square:
+""""
+classdefine a square.
+"""
+
+
+class Square:
+    """Class square define a empty value"""
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,11 @@
-class my_square:
+#!/usr/bin/python3
+""""
+classdefine a square.
+"""
+
+
+class Square:
+    """Class square define a empty value"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -6,5 +13,3 @@ class my_square:
             raise ValueError("size must be >= 0")
         else:
             self.size = size
-
-Square = my_square
