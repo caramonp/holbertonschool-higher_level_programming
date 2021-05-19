9f31430ed8197949df688337a63a0f5a162b4ba5
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-19 03:16:55 +0000

    objet position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""""
+classdefine a square.
+"""
+
+
+class Square:
+    """Class square define a  value"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if len(value) != 2 and type(value) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """method area to multiplicate size for himself"""
+        size = self.__size
+        return size * size
+
+    def my_print(self):
+        size = self.__size
+        position = self.__position
+
+        for row in range(size):
+            print(" " * position[0] + "#" * size)
+
+        if size == 0:
+            print()
+        if position[1] > 0:
+            print()
