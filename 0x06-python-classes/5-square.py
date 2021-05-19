d2fd0b35fb194add63ced2d103c37a040be47bcd
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-19 02:41:37 +0000

    replace # * size-

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""""
+classdefine a square.
+"""
+
+
+class Square:
+    """Class square define a  value"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """method area to multiplicate size for himself"""
+        size = self.__size
+        return size * size
+
+    def my_print(self):
+        size = self.__size
+
+        print("#"*size)
+
+        if size == 0:
+            print()
