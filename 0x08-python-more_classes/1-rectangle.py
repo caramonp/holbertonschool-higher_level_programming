9e5bf446f34f80dcc2849cf56063298cc8c94043
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-22 20:16:18 +0000

    commitments

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
     def width(self, value):
         """ Setter for width
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -39,7 +39,7 @@ class Rectangle:
     def height(self, value):
         """ Setter for height
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
