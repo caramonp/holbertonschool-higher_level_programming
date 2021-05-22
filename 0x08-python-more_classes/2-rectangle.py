468d4e7ebfd874affc91cc634497c9ac8dd4166a
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-22 20:18:18 +0000

    corrections

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 """"
-classdefine a rectabgle.
+class define a rectabgle.
 """
 
 
 class Rectangle:
-    """Class my_rectangledefine a empty value"""
+    """Class my_rectangle define a empty value
+    """
     def __init__(self, width=0, height=0):
+        """ Instantiation
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """ Getter for width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Setter for width """
+        """ Setter for width
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,16 +31,20 @@ class Rectangle:
 
     @property
     def height(self):
+        """ Getter for height
+        """
         return self.__height
 
     @width.setter
     def height(self, value):
-        """ Setter for height """
+        """ Setter for height
+        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
 
     def area(self):
         """ Returns area of rectangle"""
