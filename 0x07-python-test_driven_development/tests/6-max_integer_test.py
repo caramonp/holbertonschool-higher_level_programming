f92e86868107afb238f3231bc9a8cdc0641840d6
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-05-26 05:03:27 +0000

    max interger

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,9 +8,7 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxIntegrer(unittest.TestCase):
     
     def test_max_integer(self):
-        list = [1, 2, 4, 3]
-        max_integer([list])
-        self.assertEqual(list.max_integrer, 4)
+        self.assertEqual(max_integer([1, 2, 4]), 4)
 
 
 if __name__ == '__main__':
