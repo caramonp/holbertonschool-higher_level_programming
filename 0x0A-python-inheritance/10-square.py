74e4fa355dc6f1cccd0761517c50189a7dda763d
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-06-02 03:28:40 +0000

    square inheret reactangule who inheret BaseGeometry

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Square inheriting Rectangle class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class Square
+    """
+    def __init__(self, size):
+        """Initilization class attributes
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return Rectangle.area(self)
