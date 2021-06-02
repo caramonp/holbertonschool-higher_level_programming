faa0956a5d1f4d2208540cfa380de9f3e6936bb4
caramonp <carolinaramon239578@correo.itm.edu.co>
2021-06-02 02:54:46 +0000

    def area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry empty class
+    """
+    def area(self):
+        """Function that calculates the area
+        """
+        raise Exception("area() is not implemented")
+
