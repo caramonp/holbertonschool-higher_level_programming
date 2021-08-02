5a9e703b2c66bdda97d95f13afd72ec9d3a3b6cb
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-02 16:38:10 -0500

    script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const argument = process.argv[2];
+
+if (!isNaN(parseInt(process.argv[2]))) {
+  for (let i = 0; i < argument; i++) {
+    let row = '';
+    for (let j = 0; j < argument; j++) {
+      row += 'x';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
