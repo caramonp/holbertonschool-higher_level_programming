41e4db97f1ca0b28823cfcb83145474428e7b80c
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-02 12:20:11 -0500

    script that prints two arguments passed to it, in the following format: “ is ”

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined) {
+  console.log('undefined is undefined');
+} else if (process.argv[2] && process.argv[3]) {
+  console.log(process.argv[2], 'is', process.argv[3]);
+} else {
+  console.log(process.argv[2], 'is', 'undefined');
+}
