53dc8fb7e6c79c6e64fddf7f55c7be46b0110a90
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-02 19:55:50 -0500

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length < 3) {
+const argumt = process.argv.slice(2);
+if (argumt.length < 1) {
   console.log('No argument');
 } else {
   console.log('Argument found');
