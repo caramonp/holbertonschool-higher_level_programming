9c81dfb1b397cfc59bead454ad99417d6fe8a5c7
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-06 16:18:41 -0500

    readme file and point cero

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""[ lists all states from the database hbtn_0e_0_usa]
+"""
+import sys
+import MySQLdb
+
+
+
+if __name__ == "__main__":
+  
+  #Open DataBase
+  db = MySQLdb.connect(host="localhost", port=3306, user= sys.argv[1], passwd= sys.argv[2], db=sys.argv[3])
+  
+  #Create a object with cursor method
+  cursor = db.cursor()
+  
+  ## execute SQL query using execute() method
+  cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+  #Fetch a single row using fetchone() method
+  for row in cursor.fetchall():
+      print(row)
+
+  ## disconnect from server
+  cursor.close()
+  db.close()
