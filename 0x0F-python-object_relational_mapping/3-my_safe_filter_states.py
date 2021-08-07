6b92258357de1d15b871c783dab70261d906c902
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-07 08:28:22 -0500

    script that takes in arguments and displays all values in the       states table of hbtn_0e_0_usa safe from MySQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""[ script that takes in arguments and displays all values in the
+      states table of hbtn_0e_0_usa safe from MySQL injections!]
+"""
+import sys
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    # Open DataBase
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    # Create a object with cursor method
+    cursor = db.cursor()
+
+    # execute SQL only recive %s
+    cursor.execute("SELECT * FROM states WHERE name= '%s'\
+    ORDER BY id ASC".format(argv[4], ))
+
+    # Fetch a single row using fetchone() method
+    for state in cursor.fetchall():
+        if state[1] == argv[4]:
+            print(state)
+
+    # disconnect from server
+    cursor.close()
+    db.close()
