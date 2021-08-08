2c45c5e1f1d663236c955e3791e2f7eece017e24
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-07 21:30:11 -0500

    cript that adds the State object “Louisiana” to the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""[script that adds the State object “Louisiana” 
+    to the database hbtn_0e_6_usa]
+"""
+
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+
+    engine = create_engine(url.format(user, password, db),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+    session.close()
