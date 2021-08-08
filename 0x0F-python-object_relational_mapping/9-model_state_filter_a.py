4d0114fd37f9d67cb480c8063eb474b70ef93500
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-07 21:07:40 -0500

    script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""[script that lists all State objects that contain 
+"""[script that lists all State objects that contain
     the letter a from the database hbtn_0e_6_usa]
 """
 
