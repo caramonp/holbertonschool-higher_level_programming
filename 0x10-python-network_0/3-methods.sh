acd2f509680a55254ae166145fe00276f3bd0e1e
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-19 17:19:05 -0500

    Bash script that takes in a URL and displays all HTTP methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods 
-curl -s -i -X OPTIONS "$1" | grep Allow | cut -d' ' -f 2-
+curl -s -i -X OPTIONS "$1" | grep Allow | cut -d " " -f 2-
