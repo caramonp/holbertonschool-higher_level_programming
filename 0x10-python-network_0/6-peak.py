b3edce28e7b76260b92b875f9a974275909a2464
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-08-19 18:22:29 -0500

    find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """Find a peak
+    """
+    lista_ordenada = sorted(list_of_integers)
+    for i, number in enumerate(lista_ordenada):
+        return (lista_ordenada[i-1])
