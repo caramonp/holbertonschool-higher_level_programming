4fea7595bd13b8cbc5041ee602ae7d27dadad35d
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-14 18:06:10 -0500

    display status code of get request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//  script that display the status code of a GET request
+
+require('request').get(process.argv[2], (err, r) => {
+  if (err) console.log(err);
+  else console.log('code:' + r.statusCode);
+});
