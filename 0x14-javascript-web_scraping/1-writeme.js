a71b353f5208efc2187e956c1e4ec42ad49fa674
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-14 17:46:45 -0500

    script that writes a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that writes a string to a file
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+  if (err) throw err;
+});
